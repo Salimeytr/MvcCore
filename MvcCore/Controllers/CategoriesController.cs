9a45c2264aaa49f5ab6a1178e907211b0e2609fd
Salimeytr <salimeyeter@outlook.com>
2022-02-17 18:41:29 +0300

    Add project files.

---
+++ MvcCore/Controllers/CategoriesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCore.Models.Classes;
+using MvcCore.Models.Data;
+using System.Linq;
+
+namespace MvcCore.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        ProductContext _db;
+        Categories _categories;
+
+        public CategoriesController(ProductContext db,Categories categories)
+        {
+            _db = db;
+            _categories = categories;
+        }
+        public IActionResult List ()
+        {
+            return View(_db.Set<Categories>().ToList());
+
+        }
+        [HttpGet]
+        public IActionResult Create ()
+        {
+            //Categories cat = new Categories();
+            //return View(cat);
+            return View(_categories);
+
+        }
+        [HttpPost]
+        public IActionResult Create(Categories model)
+        {
+            _db.Set<Categories>().Add(model);
+            _db.SaveChanges();
+             return RedirectToAction("List"); 
+
+        }
+
+
+    }
+}
---
+++ MvcCore/Models/Data/ProductsContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcCore.Models.Classes;
+
+namespace MvcCore.Models.Data
+{
+    public class ProductContext : DbContext
+
+    {
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Categories> Categories { get; set; }
+        
+
+
+    }
+
+    
+   
+
+}
